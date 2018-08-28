c88bdf35d84aee35372296a5f5e444498002dade
Badika <victoria.figurska0@gmail.com>
2018-08-28 18:26:08 +0300

    1

---
+++ MyDraw/MyDraw/FingerDrawView.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace MyDraw
+{
+    class FingerPaintPolyline
+    {
+        public FingerPaintPolyline()
+        {
+            Path = new Path();
+        }
+
+        public Color _Color { set; get; }
+        public float Size { set; get; }
+        public Path Path { private set; get; }
+    }
+
+    public class FingerDrawView : View
+    {
+        Dictionary<int, FingerPaintPolyline> inProgressPolylines = new Dictionary<int, FingerPaintPolyline>();
+        List<FingerPaintPolyline> completedPolylines = new List<FingerPaintPolyline>();
+        Paint paint = new Paint();
+
+        // External interface accessed from MainActivity
+        public Color LineColor { set; get; } = Color.Red;
+        public float LineSize { set; get; } = 1;
+
+        public FingerDrawView(Context context) : base(context)
+        {
+           
+        }
+        public FingerDrawView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+
+        }
+
+        public void ClearAll()
+        {
+            completedPolylines.Clear();
+            Invalidate();
+        }
+
+        public void Undo()
+        {
+            completedPolylines.RemoveAt(completedPolylines.Count-1);
+            Invalidate();
+        }
+
+        // Overrides
+        public override bool OnTouchEvent(MotionEvent args)
+        {
+            // Get the pointer index
+            int pointerIndex = args.ActionIndex;
+
+            // Get the id to identify a finger over the course of its progress
+            int id = args.GetPointerId(pointerIndex);
+
+            // Use ActionMasked here rather than Action to reduce the number of possibilities
+            switch (args.ActionMasked)
+            {
+                case MotionEventActions.Down:
+                case MotionEventActions.PointerDown:
+
+                    // Create a Polyline, set the initial point, and store it
+                    FingerPaintPolyline polyline = new FingerPaintPolyline
+                    {
+                        _Color = LineColor,
+                        Size = LineSize
+                    };
+
+                    polyline.Path.MoveTo(args.GetX(pointerIndex),
+                                         args.GetY(pointerIndex));
+
+                    inProgressPolylines.Add(id, polyline);
+                    break;
+
+                case MotionEventActions.Move:
+
+                    // Multiple Move events are bundled, so handle them differently
+                    for (pointerIndex = 0; pointerIndex < args.PointerCount; pointerIndex++)
+                    {
+                        id = args.GetPointerId(pointerIndex);
+
+                        inProgressPolylines[id].Path.LineTo(args.GetX(pointerIndex),
+                                                            args.GetY(pointerIndex));
+                    }
+                    break;
+
+                case MotionEventActions.Up:
+                case MotionEventActions.Pointer1Up:
+
+                    inProgressPolylines[id].Path.LineTo(args.GetX(pointerIndex),
+                                                        args.GetY(pointerIndex));
+
+                    // Transfer the in-progress polyline to a completed polyline
+                    completedPolylines.Add(inProgressPolylines[id]);
+                    inProgressPolylines.Remove(id);
+                    break;
+
+                case MotionEventActions.Cancel:
+                    inProgressPolylines.Remove(id);
+                    break;
+            }
+
+            // Invalidate to update the view
+            Invalidate();
+
+            // Request continued touch input
+            return true;
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+
+            // Clear canvas to white
+            paint.SetStyle(Paint.Style.Fill);
+            paint.Color = Color.White;
+            canvas.DrawPaint(paint);
+
+            // Draw strokes
+            paint.SetStyle(Paint.Style.Stroke);
+            paint.StrokeCap = Paint.Cap.Round;
+            paint.StrokeJoin = Paint.Join.Round;
+
+            // Draw the completed polylines
+            foreach (FingerPaintPolyline polyline in completedPolylines)
+            {
+                paint.Color = polyline._Color;
+                paint.StrokeWidth = polyline.Size;
+                canvas.DrawPath(polyline.Path, paint);
+            }
+
+            // Draw the in-progress polylines
+            foreach (FingerPaintPolyline polyline in inProgressPolylines.Values)
+            {
+                paint.Color = polyline._Color;
+                paint.StrokeWidth = polyline.Size;
+                canvas.DrawPath(polyline.Path, paint);
+            }
+        }
+    }
+}
---
+++ MyDraw/MyDraw/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
+
+namespace MyDraw
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private FingerDrawView fingerDrawView;
+        private SeekBar colorBar;
+        private SeekBar sizeBar;
+        private Button clearButton;
+        private Button undoButton;
+        private Button imageButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            FindResources();
+            Initialize();
+            AddEvents();
+            LinearGradient test = new LinearGradient(0, 0, 1000, 0, new int[] { Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Violet }, null, Shader.TileMode.Clamp);
+            ShapeDrawable shape = new ShapeDrawable(new RectShape());
+            shape.Paint.SetShader(test);
+
+            colorBar.SetProgressDrawableTiled(shape);
+        
+        }
+
+    
+
+        private void FindResources()
+        {
+            fingerDrawView = FindViewById<FingerDrawView>(Resource.Id.myFingerDrawView);
+            colorBar = FindViewById<SeekBar>(Resource.Id.seekbar_color);
+            sizeBar = FindViewById<SeekBar>(Resource.Id.seekbar_size);
+            clearButton = FindViewById<Button>(Resource.Id.clear_button);
+            undoButton = FindViewById<Button>(Resource.Id.undo_button);
+            imageButton = FindViewById<Button>(Resource.Id.load_image);
+        }
+        private void AddEvents()
+        {
+            colorBar.ProgressChanged += ColorBar_ProgressChanged;
+            sizeBar.ProgressChanged += SizeBar_ProgressChanged;
+            clearButton.Click += ClearButton_Click;
+            undoButton.Click += UndoButton_Click;
+        }
+
+        private void UndoButton_Click(object sender, System.EventArgs e)
+        {
+            fingerDrawView.Undo();
+        }
+
+        private void Initialize()
+        {
+
+
+        }
+
+        private void ClearButton_Click(object sender, System.EventArgs e)
+        {
+            fingerDrawView.ClearAll();
+        }
+        private void SizeBar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            SeekBar seekbar = (SeekBar)sender;
+            double size = (double)seekbar.Progress*2;
+            fingerDrawView.LineSize = (float)size;
+
+        }
+        private void ColorBar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        { 
+            //////////////////////////////////////////////
+            SeekBar seekbar = (SeekBar)sender;
+            ColorDrawable color = (ColorDrawable)seekbar.ProgressDrawable;
+            fingerDrawView.LineColor = color.Color;
+        }
+    }
+}
